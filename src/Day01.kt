993a25202175c130ab34879fd54b984a51d6b0f1
Rémi Duriez <remi.duriez.partner@decathlon.com>
2022-12-02 17:32:40 +0100

    Resolve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var best = 0
+        var total = 0
+        for (line in input) {
+            if (line.isBlank()) {
+                if (total > best) best = total
+                total = 0
+            } else {
+                total += line.toInt()
+            }
+        }
+        return best
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val best3 = intArrayOf(0, 0, 0)
+        var total = 0
+        for (line in input) {
+            if (line.isBlank()) {
+                if (total > best3[0]) best3[0] = total
+                best3.sort()
+                total = 0
+            } else {
+                total += line.toInt()
+            }
+        }
+        return best3.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
